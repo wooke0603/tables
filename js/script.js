8047c35af72fd89a09ec27d338659dd03b28c9d7
wooke <wooke0603@naver.com>
2019-09-23 23:24:35 +0900

    winload.js 추가

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 ;(function($){
     $(function(){
-
+        // .menubtn 클릭시 할 일. 
         $('.menubtn').click(function(){
+            // #gnb 클릭시 menuon 클래스 추가
             $('#gnb').addClass('menuon');
         });
 
+        //#gnb button 클릭시 할 일.
         $('#gnb button').click(function(){
+            //#gnb 클릭시 menuon 클래스 삭제
             $('#gnb').removeClass('menuon');
         });
 
---
+++ js/winload.js
@@ -0,0 +1,7 @@
+//변수 선언
+let skills = document.querySelector('.opacity');
+console.log(skills);
+// 윈도우 로드될시 할 일
+window.onload = function() {
+    skills.style.opacity = "1";
+}
